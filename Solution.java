e8c392b1ca54a6cd187c518e239c72b054800ce3
Lachezar Kutsarov <lkutsarov@gmail.com>
2023-05-28 12:56:42 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,1 +1,9 @@
 
+public class Solution {
+
+    public int maximumScore(int a, int b, int c) {
+        int maxValue = Math.max(a, Math.max(b, c));
+        int totalSum = a + b + c;
+        return maxValue >= totalSum - maxValue ? (totalSum - maxValue) : totalSum / 2;
+    }
+}
